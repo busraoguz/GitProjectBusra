a15a6449bf876a6c4f4b75f3e6ada3d317825073
busraoguz <busraaoguz@outlook.com>
2022-10-13 10:44:04 -0400

    islem tamamlandi

--- src/Merhaba.java
+++ src/Merhaba.java
@@ -1,5 +1,8 @@
 public class Merhaba {
     public static void main(String[] args) {
         System.out.println("Merhaba");
+        for (int i = 0; i <6 ; i++) {
+            System.out.println("geldim");
+        }
     }
 }
